04901951cd9d9a43bb53eb095de056cd8bb94bb1
Manjunath-Bhashyam <manjunathbhashyam1995@gmail.com>
2022-10-17 00:05:57 +0530

    organized script

--- and.py
+++ and.py
@@ -1,22 +1,31 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1]
-}
+def main(data,eta,epochs,filename,PlotFilename):
+    
+    df = pd.DataFrame(data)
+    print(df)
 
-df = pd.DataFrame(AND)
-print(df)
+    X,y = prepare_data(df)
 
-X,y = prepare_data(df)
+    model = Perceptron(eta = eta, epochs = epochs)
+    model.fit(X,y)
 
-ETA = 0.3 # 0 to 1
-EPOCHS = 10
+    _ = model.total_loss() # _ is a dummy variable
 
-model = Perceptron(eta = ETA, epochs = EPOCHS)
-model.fit(X,y)
+    save_model(model, filename=filename)
+    save_plot(df, PlotFilename, model)
 
-_ = model.total_loss()
+if __name__ == '__main__': # entry point
+
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1]
+    }
+
+    ETA = 0.3 # 0 to 1
+    EPOCHS = 10
+
+    main(data=AND, eta=ETA, epochs=EPOCHS, filename="and.model", PlotFilename="and.png")
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seborn as sns
+import seaborn as sns
 import joblib
 import os
 from matplotlib.colors import ListedColormap
