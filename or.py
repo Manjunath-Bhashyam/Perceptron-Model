c12713b8ab7ef2e48c92218c15ead7178b5e666e
Manjunath-Bhashyam <manjunathbhashyam1995@gmail.com>
2022-10-29 22:41:49 +0530

    basic logging added

--- or.py
+++ or.py
@@ -2,11 +2,19 @@ import pandas as pd
 import numpy as np
 from utils.all_utils import prepare_data, save_model, save_plot
 from utils.model import Perceptron
+import logging
+import os
+
+
+logging_str = "[%(asctime)s - %(levelname)s - %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str)
 
 def main(data,eta,epochs,filename,PlotFilename):
     
     df = pd.DataFrame(data)
-    print(df)
+    logging.info(f"This is actual DataFrame{df}")
 
     X,y = prepare_data(df)
 
@@ -29,4 +37,10 @@ if __name__ == '__main__': # entry point
     ETA = 0.3 # 0 to 1
     EPOCHS = 10
 
-    main(data=OR, eta=ETA, epochs=EPOCHS, filename="or.model", PlotFilename="or.png")
+    try:
+        logging.info(">>>>> Starting Training here <<<<<")
+        main(data=OR, eta=ETA, epochs=EPOCHS, filename="or.model", PlotFilename="or.png")
+        logging.info(">>>>>Training done successfully<<<<<")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -5,10 +5,20 @@ import seaborn as sns
 import joblib
 import os
 from matplotlib.colors import ListedColormap
+import logging
 
 plt.style.use("fivethirtyeight")
 
 def prepare_data(df):
+  """It is used to separate dependent features and independent features
+
+  Args:
+      df (pd.DataFrame): its the pandas DataFrame to
+
+  Returns:
+      tuple: it returns the tuple of dependent features and independent features.
+  """
+  logging.info(f"Preparing the data by segregating Independent and Dependent features")
   X = df.drop("y", axis=1)
   y = df["y"]
   return X, y
@@ -16,16 +26,32 @@ def prepare_data(df):
 # Saving Perceptron Model
 
 def save_model(model, filename):
+  """This saves the model to
+
+  Args:
+      model (python object): trained model to
+      filename (str): path to save the trained model
+  """
+  logging.info(f"Saving the trained model")
   model_dir = "models"
   os.makedirs(model_dir,exist_ok=True) # Only create if model_dir diesn't exist
   filePath = os.path.join(model_dir, filename) # creates models\filename
   
   joblib.dump(model,filePath)
+  logging.info(f"Saved the trained model at {filePath}")
 
 # Saving plot
 
 def save_plot(df, file_name, model):
+  """It saves a plot to the
+
+  Args:
+      df (pd.DataFrame): its a DataFrame
+      file_name (str): path to save the plot
+      model (pkl or .model): trained model
+  """
   def _create_base_plot(df):
+    logging.info(f"Creating the base plot")
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter") #Pandas direct plot method c means color level outcome s-dia of circle
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
     plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -33,6 +59,8 @@ def save_plot(df, file_name, model):
     figure.set_size_inches(10,8)
 
   def _plot_decision_regions(X, y, classifier, resolution=0.02):
+    logging.info(f"Plotting decision regions")
+    figure = plt.figure(figsize=(8,8))
     colors = ("red","blue","lightgreen","gray","cyan")
     cmap = ListedColormap(colors[: len(np.unique(y))])
 
@@ -44,8 +72,8 @@ def save_plot(df, file_name, model):
 
     xx1, xx2 = np.meshgrid(np.arange(x1_min,x1_max,resolution), np.arange(x2_min,x2_max,resolution))
 
-    print(xx1)
-    print(xx1.ravel())
+    #print(xx1)
+    #print(xx1.ravel())
 
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
@@ -64,3 +92,5 @@ def save_plot(df, file_name, model):
   os.makedirs(plot_dir, exist_ok=True) # only create if plot_dir doesn't exists
   plotpath = os.path.join(plot_dir, file_name) # model/filename
   plt.savefig(plotpath)
+
+  logging.info(f"Saving the plot at {plotpath}")
